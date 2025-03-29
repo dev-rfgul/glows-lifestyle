29d0ab32c6256258c1844268c0b0e5405ac67345
dev-rfgul <rfgul587@gmail.com>
2025-03-29 21:58:33 +0500

    removed unnecessary code from the backend files as it was creating issues in the backend deployment

--- backend/index.js
+++ backend/index.js
@@ -3,20 +3,11 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
-import{auth } from 'express-openid-connect';
 
 
 
 // Load environment variables first
 dotenv.config();
-const config = {
-    authRequired: false,
-    auth0Logout: true,
-    secret: process.env.SECRET,
-    baseURL: process.env.BASE_URL,
-    clientID: process.env.CLIENT_ID,
-    issuerBaseURL: process.env.ISSUER_BASE_URL,
-};
 
 
 import { connectDB } from './config/db.js';
@@ -26,12 +17,11 @@ import adminRoutes from './routes/admin.routes.js';
 import paymentRoutes from './routes/payment.routes.js'
 import AnalyticsRoutes from './routes/analytics.routes.js'
 import { cloudinaryConnect } from './config/cloudinary.js';
-import authRoutes from './routes/auth.routes.js'
 // Initialize app and services
 const app = express();
 
 // Properly handle CORS origins
-const allowedOrigins = ["https://dewnor-frontend.onrender.com", "https://dewnor2.vercel.app"];
+const allowedOrigins = ["https://glowslifestyle.onrender.com", ];
 if (process.env.FRONT_END_URL) {
     allowedOrigins.push(process.env.FRONT_END_URL);
 }
@@ -47,7 +37,6 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use(cookieParser());
-app.use(auth(config))
 
 // for google authentication
 
@@ -81,18 +70,9 @@ app.use('/product', productRoutes);
 app.use('/admin', adminRoutes);
 app.use('/payment', paymentRoutes)
 app.use('/analytics', AnalyticsRoutes)
-app.use('/auth', authRoutes)
 
 
 
-
-// auth router attaches /login, /logout, and /callback routes to the baseURL
-app.use(auth(config));
-
-// req.isAuthenticated is provided from the auth router
-app.get('/', (req, res) => {
-    res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
-});
 
 
 // Start server with proper port fallback
