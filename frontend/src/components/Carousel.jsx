256963ab4a4a963e0c7ebf1165c116eb47fb0eca
dev-rfgul <rfgul587@gmail.com>
2025-05-05 22:00:46 +0500

    changed the style of the banners

--- frontend/src/components/Carousel.jsx
+++ frontend/src/components/Carousel.jsx
@@ -10,7 +10,7 @@
 //         "./images/carousel/banner-1.jpeg",
 //         // "./images/carousel/banner-2.jpeg",
 //         // "./images/carousel/banner-3.jpeg",
-   
+
 //     ];
 
 //     const settings = {
@@ -64,8 +64,10 @@ import "slick-carousel/slick/slick-theme.css";
 
 const ImageCarouselWithStaticBanner = () => {
     const carouselImages = [
-        "./images/carousel/banner-4.jpeg",
-        "./images/carousel/banner-1.jpeg",
+        "/images/carousel/banner-4.jpeg",
+        "/images/carousel/banner-1.jpeg",
+        // "/images/carousel/banner-2.jpeg",
+        // "/images/carousel/banner-3.jpeg",
     ];
 
     const settings = {
@@ -80,28 +82,28 @@ const ImageCarouselWithStaticBanner = () => {
     return (
         <div className="w-full px-4 py-6 max-w-7xl mx-auto">
             <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-                {/* Carousel (left) */}
-                <div className="md:col-span-2 overflow-hidden rounded-md bg-white">
+                {/* Carousel (left side) */}
+                <div className="md:col-span-2 h-[400px] sm:h-[450px] md:h-[500px] lg:h-[550px] overflow-hidden rounded-md">
                     <Slider {...settings}>
                         {carouselImages.map((img, index) => (
                             <div
                                 key={index}
-                                className="flex items-center justify-center w-full"
+                                className="w-full h-[400px] sm:h-[450px] md:h-[500px] lg:h-[550px] relative"
                             >
                                 <img
                                     src={img}
                                     alt={`slide-${index}`}
-                                    className="w-full h-auto object-contain rounded-md"
+                                    className="absolute top-0 left-0 w-full h-full object-contain"
                                 />
                             </div>
                         ))}
                     </Slider>
                 </div>
 
-                {/* Static Banner (right) - Hidden on mobile */}
-                <div className="hidden md:block h-[500px] rounded-md overflow-hidden">
+                {/* Static Banner (right side, hidden on small screens) */}
+                <div className="hidden md:block h-[400px] sm:h-[450px] md:h-[500px] lg:h-[550px] rounded-md overflow-hidden">
                     <img
-                        src="./images/carousel/static-banner.jpeg"
+                        src="/images/carousel/static-banner.jpeg"
                         alt="static banner"
                         className="w-full h-full object-cover"
                     />
