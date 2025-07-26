14776b87ca45061e3f2affad79779a17a969e6bc
dev-rfgul <rfgul587@gmail.com>
2025-07-26 11:20:06 +0500

    refactor: adjust layout and styling of TextScroll component in Home

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -18,14 +18,20 @@ const Home = () => {
             <Carousel />
             <Heading heading={"Flash Sale"} subHeading={"Purchase It, Before it run out of stock "} />
 
-            <CardCarousel/>
-            <ProductCarousel />
+            <CardCarousel />
             <FeatureSection />
+            <ProductCarousel />
             <Heading heading={"Explore"} subHeading={"Explore our wide range of products"} />
             <Explore />
             <Heading heading={"Featured Products"} subHeading={"Check out our Featured Products"} />
             <ProductCarousel />
-            <TextScroll text="Shop Now Pay Later , No Fee" default_velocity={3} className="text-8xl font-bold text-center my-4" />
+            <div className="w-full px-4 md:px-8">
+                <TextScroll
+                    text="Shop Now Pay Later , No Fee"
+                    default_velocity={3}
+                    className="text-2xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-8xl font-bold text-center my-4"
+                />
+            </div>
             <FeaturedProducts />
 
 
